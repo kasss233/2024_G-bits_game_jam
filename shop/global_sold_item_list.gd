5334650a15b3ddead5140cc44870f6ce6316e345
zhitian111 <--global>
2024-11-25 12:51:53 +0800

    audioplayer and shop

---
+++ shop/global_sold_item_list.gd
@@ -0,0 +1,39 @@
+extends Node
+
+
+enum Pets
+{
+	Cat,
+	Dog,
+	Bird,
+	Mouse,
+	Snake
+};
+
+enum PenTools
+{
+	Pencil,
+	Pen,
+	Eraser
+};
+
+enum Sports
+{
+	Basketball,
+	Football,
+	Baseball,
+	Tennis
+};
+
+enum Weapons
+{
+	USP,
+	Glock,
+	AK47,
+	Neigfur,
+	YDLPao,
+	WitcherSword,
+	CyberVSword,
+	Taijutsu,
+	SubmachineGun
+};
