3ac00d4bd00d96e1b6f2cd2b050215f493ffbc78
kasssssssssss <1592858973@qq.com>
2024-11-30 23:15:53 +0800

    v1.3

--- ui/choose/choose_weapon.gd
+++ ui/choose/choose_weapon.gd
@@ -5,5 +5,5 @@ func _on_button_pressed() -> void:
 	#get_tree().change_scene_to_packed(world)
 	SceneChanger.change_scene(world)
 func _ready() -> void:
-	if GlobalVal.day_early_eight:
+	if GlobalVal.early_eight:
 		label.text="因为明天的早八，睡眠收到影响（梦魇更加活跃）"
