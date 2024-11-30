8ab9c89c7d1414b329c42f061c39840acf770e8b
kasssssssssss <1592858973@qq.com>
2024-11-30 22:27:59 +0800

    v1.2选择武器文本，胜利面板失败面板微调

--- ui/board/victory_board.gd
+++ ui/board/victory_board.gd
@@ -4,11 +4,11 @@ func _ready() -> void:
 	effect()
 	AudioPlayer.button_se_init(self)
 func _on_button_pressed() -> void: ## 更换场景，产生增益
-	if !GlobalVal.weekday == GlobalVal.week.LASTDAY:
-		GlobalVal.add_day()
-		SceneChanger.change_scene(load("res://home/home.tscn"))
-	else:
+	if GlobalVal.weekday == GlobalVal.week.LASTDAY:
 		SceneChanger.change_scene(load("res://end/end.tscn"))
+		
+	GlobalVal.add_day()
+	SceneChanger.change_scene(load("res://home/home.tscn"))
 func effect():
 	GlobalVal.add_property("stamina", 1)
 	GlobalVal.add_property("mood", 1)
